5f5503a9c20769de40da9c2ca6f121e45b15c31b
melaniede <debarrosmelanie@gmail.com>
2020-10-18 19:24:07 -0400

    add Role

--- employee.js
+++ employee.js
@@ -18,7 +18,8 @@ userPropmt = (connection) => {
             choices: [
                 "Add Department",
                 "Add Employee",
-                "Add Role"
+                "Add Role",
+                "Exit"
             ]
         }
     ]).then(function (answers) {
@@ -26,23 +27,69 @@ userPropmt = (connection) => {
             case "Add Department":
                 addDepartment();
                 break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Exit":
+                connection.end();
         }
     })
 };
 
-addDepartment = () => {
-    inquirer.prompt({
-        name: "addDepartment",
+
+addRole = () => {
+    connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    inquirer.prompt([{
+        name: "addRole",
         type: "input",
-        message: "What department would you like to add?"
+        message: "What role would you like to add?"
+    },
+    {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for this role?"
+    },
+    {
+       name: "chooseDept",
+       type: "rawlist",
+       message: "Which department does this role belong in?",
+       choices: res.map(function (role){
+           return {
+               name: role.name,
+               value: role.id
+           }
+       })
+    }]).then(function (answers){
+        connection.query("INSERT INTO role SET ?", {
+            title: answers.addRole, 
+            salary: answers.salary, 
+            department_id: answers.chooseDept
+            }, 
+            function (err, res){
+                if (err) throw err;
+                console.log("New role successfully added");
+                userPropmt(connection);
+            });
+        });
+    })
+}
+
+
+addEmployee = () => {
+    inquirer.prompt({
+        name: "addEmployee",
+        type: "input",
+        message: "Which employee would you like to add?"
     })
     .then(function(answers){
-        connection.query("INSERT INTO department (name) VALUES (?)", answers.addDepartment, function (err, res){
+        connection.query("INSERT INTO employee (name) VALUES (?)", answers.addEmployee, function (err, res){
             if (err) throw err;
-            console.log("New department successfully added");
+            console.log("New employee successfully added");
             userPropmt(connection);
         });
     })
 }
 
 userPropmt();
+
